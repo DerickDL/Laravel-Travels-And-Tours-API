b6905a9bafd052d7d9144443c03e04e4d265802e
John Derick De Leon <johnderickdeleon@gmail.com>
2023-07-08 14:41:43 +0800

    Create public endpoint to get all public travels record

---
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Travel;
+use App\Http\Resources\TravelResource;
+
+class TravelController extends Controller
+{
+    /**
+     * Get all public travels
+     */
+    public function index()
+    {
+        return TravelResource::collection(Travel::where('is_public', true)->get());
+    }
+}
